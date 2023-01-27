66089d229b91ac1369d16c58761fcde1ff6cba9a
Malhar Lathkar <mlathkar@gmail.com>
2023-01-27 23:57:43 +0530

    Model serializer example

--- DRFProject/DRFApp/views.py
+++ DRFProject/DRFApp/views.py
@@ -3,7 +3,15 @@ from django.shortcuts import render
 # Create your views here.
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .models import Book
+from .serializers import BookSerializer
 
 @api_view()
 def hello_world(request):
     return Response({"message": "Hello, world!"})
+
+@api_view() 
+def books(request): 
+    books = Book.objects.all()
+    serialized_books = BookSerializer(books, many=True)
+    return Response(serialized_books.data)
